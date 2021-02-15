0ac280108f74d163e78216db0975e9dfb93752fd
Tobias Granbom <Busky@localhost.localdomain>
2021-02-15 02:16:48 +0100

    Created my first "todo"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
+import React, {Component} from 'react' ;
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends Component {
+
+  
+  // STATE <-------------------------------------------------
+  state = {
+    myList : [],
+    myString : '',
+    test: 'This is a test',
+
+  }
+
+  handleNewTodo(event) {
+    this.setState({
+      myString : event.target.value,
+    })
+    console.log(event.target.value);
+  }
+  saveTodo(event) {
+    event.preventDefault();
+    console.log('Form Submitted!');
+    console.log('Event.target.value is: ' + event.target.vaule);
+    console.log('state.myString: ' + this.state.myString);
+
+    if (this.state.myString !== null || this.state.myString.length !== 0) {
+      // Sparar hur många som finns i state.mylist
+      let count  = this.state.myList.length; 
+
+      // Lägger till ny todo i state.mylist
+      // this.state.myList.push(this.state.myString)
+      //använder setState istället för att göra det async
+      this.setState({
+        // ... betyder att vi skapar en kopia av den gamla arrayen och lägger till det nya
+        myList : [...this.state.myList,  this.state.myString]
+      });
+      // Kolla om  det lagts till ny i state.myList
+      if (this.state.myList.length > count) {
+        console.log('ToDo added to state.myList!');
+        console.log(this.state.myList);
+      }
+    } else {
+      console.log('Error! Could not add new todo to list ');
+    }
+
+    this.reprMyList();
+  }
+  reprMyList() {
+    // console.log(this.state.myList.map());
+  }
+  // RENDER <----------------------------------------------
+  render() {
+
+    return (
+      <div className="App">
+      <header className="App-header"> 
+        <h2>
+          <ul>
+            {this.state.myList.map(todo => 
+                {
+                return <li>{todo}</li> 
+                }
+              )}
+          </ul>
+        </h2>
+        <div>
+          <form onSubmit={(event) => this.saveTodo(event)}>
+            <input onChange= {(event) => {this.handleNewTodo(event)}}type="text" />
+            <button type="submit" >Add ToDo</button>
+          </form>
+        </div>
+
       </header>
-    </div>
-  );
+      </div>
+    );
+  };
 }
 
 export default App;
